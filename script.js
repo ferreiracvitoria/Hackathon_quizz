cb1631860585fd8b45ec834c82046f779917346a
Senem Kurtulmus <Senem K>
2023-10-25 22:48:38 +0200

    rectification js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const timeElement = document.getElementById('time');
 const trueButton = document.getElementById('trueButton');
 const falseButton = document.getElementById('falseButton');
 const quizContainer = document.querySelector('.quiz-container');
+const divTimer = document.getElementById('timer');
 
-let score = document.getElementById('scoreInit');
+let score = document.getElementById('score');
 let scoreIn = 0;
 
 let questionIndex = 0;
@@ -17,18 +18,7 @@ let timerInterval; // stocke l'ID de l'intervalle du minuteur
 const questions = [
     { question: "Est-ce que la Terre est plate ?", answer: false },
     { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
-    { question: "Est-ce que la Terre est plate ?", answer: false },
-    { question: "Est-ce que l'eau bout à 100 degrés Celsius ?", answer: true },
+    { question: "Est-ce que la Terre est plate ?", answer: false }
 ];
 
 console.log(questions[0].answer);
@@ -40,32 +30,42 @@ function startTimer() {
     timerInterval = setInterval(function() {
         timeLeft--;
         timeElement.textContent = timeLeft;
-        if (timeLeft == 0) {
+        if (timeLeft === 0) {
             clearInterval(timerInterval);
-            showCorrectAnswer(questionIndex); //montrer la bonne réponse : vrai ou faux
+            showCorrectAnswer(); //Montrera la bonne réponse
             questionIndex++;
-            showQuestion();
-
+            setTimeout(showQuestion, 1000);
             //laisser l'user cliquer sur qst suivante
         }
     }, 1000);
 }
 
 //afficher le bonne réponse
-function showCorrectAnswer(questionIndex) {
-    const reponseQuest= questions[questionIndex].answer;
-    alert('la bonne réponse est ' + reponseQuest);
+function showCorrectAnswer() {
+    const reponseQuest = questions[questionIndex].answer;
+    if (reponseQuest) {
+        trueButton.classList.add('correct');
+        falseButton.classList.add('incorrect');
+    } else {
+        falseButton.classList.add('correct');
+        trueButton.classList.add('incorrect');
+    }
 }
 
 //showQuestion = montrera les questions (sans random - c'est en ordre)
 function showQuestion() {
-    setTimeout(resetButtonColors,1000);
+    isAnswered = false; //Réinitialisera isAnswered
+    resetButtonColors(); //Réinitialisera les couleurs des boutons
     clearInterval(timerInterval);
     if (questionIndex < questions.length) {
         questionElement.textContent = `Question ${questionIndex + 1} : ${questions[questionIndex].question}`;
         startTimer();
     } else {
         questionElement.textContent = "Quiz terminé!";
+        trueButton.style.display = 'none'; //masque le bouton Vrai
+        falseButton.style.display = 'none'; //masque le bouton Faux
+        divTimer.style.display ='none'; //masque le timer
+
     }
 }
 
@@ -76,29 +76,41 @@ function resetButtonColors() {
     falseButton.classList.remove('incorrect');
 }
 
+let isAnswered = false;
+
 trueButton.addEventListener('click', function() {
-    if (questionIndex < questions.length) {
+    if (!isAnswered && questionIndex < questions.length) {
+        isAnswered = true;
         if (questions[questionIndex].answer) {
             trueButton.classList.add('correct');
-            score.textContent = scoreIn;
-            scoreIn++;
+            scoreIn++; // Augmentera le score en cas de bonne réponse
+            score.textContent = scoreIn; // MAJ le score affiché
         } else {
-            falseButton.classList.add('incorrect');
+            trueButton.classList.add('incorrect');
+            setTimeout(function(){
+                falseButton.classList.add('correct'); // Mettra en vert le bouton avec la bonne réponse
+            },1000);
         }
         questionIndex++;
-        showQuestion();
+        setTimeout(showQuestion, 2000);
     }
 });
 
 falseButton.addEventListener('click', function() {
-    if (questionIndex < questions.length) {
+    if (!isAnswered && questionIndex < questions.length) {
+        isAnswered = true;
         if (!questions[questionIndex].answer) {
-            falseButton.classList.add('incorrect');
+            falseButton.classList.add('correct');
+            scoreIn++; //Augmentera le score en cas de bonne réponse
+            score.textContent = scoreIn; //MAJ le score affiché
         } else {
-            trueButton.classList.add('correct');
+            falseButton.classList.add('incorrect');
+            setTimeout(function(){
+                trueButton.classList.add('correct'); // Mettre en vert le bouton avec la bonne réponse
+            }, 1000);
         }
         questionIndex++;
-        showQuestion();
+        setTimeout(showQuestion, 2000); //temps pour que l'user voir la couleur des boutons
     }
 });
 
